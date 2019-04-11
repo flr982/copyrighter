2e83505ffbf64f1626e67e30f116dc50c2432422
Florian Anderson <florian.anderson@gmail.com>
2019-04-11 16:57:56 +0000

    replacer.py updated

--- replacer.py
+++ replacer.py
@@ -8,10 +8,10 @@ import os
 from flask import send_file
 
 # make a list of names that should have the ® symbol appended
-brands = open("static/Keyword.txt",'r').read().split('\n')
 
 # append ® to each name included in the Keyword.txt file
 def rpl(text):
+    brands = open("static/Keyword.txt",'r').read().split('\n')
     for name in brands:
         text = text.replace( name, name + '\u00ae')
     if os.path.exists("output.txt"):
